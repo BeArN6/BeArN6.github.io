d3d574f7af08589c01565e6d76646c1f0f16d5dd
Oleh Savitskiy <oleh.savitskiy@gmail.com>
2017-03-23 21:18:42 +0200

    commit

---
+++ Freebie/js/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+	$(".ind-box-cont button").hover(function(){
+		$(this).stop().animate({marginTop: '40px'}, 200)
+	},
+	function(){
+		$(this).stop().animate({marginTop: '45px'}, 200)
+	});
+	//Nav menu
+	$(".nav-toggle").click(function(){
+		$(".nav-menu").slideToggle(400);
+	});
+});
