8f9ab2360705acbe799f354392d35407a9d34724
BeArN6 <oleh.savitskiy@gmail.com>
2017-08-20 20:09:22 +0300

    1

--- devilery/js/script.js
+++ devilery/js/script.js
@@ -7,4 +7,44 @@ $(document).ready(function(){
 			$("header nav ul li").not(".toggle").slideToggle(500);
 		});
 	}
+	$('body').on('click','.first',function(){
+		$(this).siblings().toggle();
+	});
+	$("body").on('click','.toggle_type p:not(.first)',function(){
+		$(this).addClass("first");
+		$(this).siblings().removeClass("first");
+		$(this).siblings().hide();
+	});
+	//slider
+		
+	if(windWidth > 1400){
+		$('.brand_cont').bxSlider({
+	    	slideWidth: 215,
+	    	minSlides: 2,
+	    	maxSlides: 6,
+	    	slideMargin: 45,
+	    	pager: false
+	  	});
+	  	$('.home_slider').bxSlider({
+			controls: false
+	  	});
+	  	$('.slider1').bxSlider({
+			controls: false
+	  	});
+	}
+  	if(windWidth < 1400){
+  		$('.brand_cont').bxSlider({
+    		slideWidth: 148,
+    		minSlides: 2,
+    		maxSlides: 6,
+    		slideMargin: 35,
+    		pager: false
+  		});
+  		$('.home_slider').bxSlider({
+			controls: false
+	  	});
+	  	$('.slider1').bxSlider({
+			controls: false
+	  	});
+  	}
 });
