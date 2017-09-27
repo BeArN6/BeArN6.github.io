2aa2ef3b6e24723161a0b2b672eb299bc283a31f
BeArN6 <oleh.savitskiy@gmail.com>
2017-09-27 13:49:58 +0300

    mc

---
+++ mrcleaner/js/script.js
@@ -0,0 +1,70 @@
+$(document).ready(function(){
+	//Modal
+	var modalWidth = $('.modal-form').width();
+	var modalFormMargin = modalWidth/2;
+
+		$('.modal-run').click(function(event){
+			event.preventDefault();
+			$(".overlay").fadeIn(0,function(){
+				$('.modal-form')
+					.css({
+						'display': 'flex',
+						'overflow-y': 'scroll',
+    					'overflow-x': 'hidden',
+					})
+					.animate({
+						'opacity': '1',
+						'left': '0'
+					},300)
+			});
+			$("body").css({
+				"overflow":"hidden"
+			})
+		});
+		$('.modal-close,.overlay').click(function(){
+			$(".modal-form").animate({
+					'opacity': '1',
+					'left': '-300px'
+				}, 300,
+				function(){
+					$(this).css('display', 'none'); 
+					$('.overlay').fadeOut(300);
+				}
+			);
+			$("body").css({
+				"overflow":"auto"
+			})
+	});
+	//Modal end
+	//Menu
+	// $(window).scroll(function(){
+ //        if ( $(this).scrollTop() > 100 && $(".header").hasClass("default") ){
+ //            $(".header").removeClass("default").addClass("fixed");
+ //            $('body').css({
+ //            	'paddingTop':'50px'
+ //            });
+ //        } else if($(this).scrollTop() <= 100 && $(".header").hasClass("fixed")) {
+ //            $(".header").removeClass("fixed").addClass("default");
+ //            $('body').css({
+ //            	'paddingTop':'0',
+ //            });
+ //        }
+ //    });
+
+	$(".question__title").click(function(){
+		$(this).siblings('.question__text').toggle();
+		$(this).toggleClass('question__title_active');
+	});
+	var windWidth = $(window).width();
+	if(windWidth < 768){
+		$('.slick_slider').slick({
+	  		infinite: true,
+	  		slidesToShow: 1,
+	  		slidesToScroll: 1,
+	  		arrows: false,
+	  		centerMode: true,
+	  		centerPadding: '60px',
+	  		variableWidth: true
+		});
+	}
+});
