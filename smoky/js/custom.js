724c9580b466917d89a0b992aa8a67e7ca633239
BeArN6 <oleh.savitskiy@gmail.com>
2017-12-08 00:34:25 +0200

    smoky

--- smoky/js/custom.js
+++ smoky/js/custom.js
@@ -35,58 +35,6 @@ $(function(){
 		$('.main-nav-list').slideToggle(); 
 		return false;
 	});
-	
-	/* components */
-	
-	/*
-	
-	if($('.styled').length) {
-		$('.styled').styler();
-	};
-	if($('.fancybox').length) {
-		$('.fancybox').fancybox({
-			margon  : 10,
-			padding  : 10
-		});
-	};
-	if($('.slick-slider').length) {
-		$('.slick-slider').slick({
-			dots: true,
-			infinite: false,
-			speed: 300,
-			slidesToShow: 4,
-			slidesToScroll: 4,
-			responsive: [
-				{
-				  breakpoint: 1024,
-				  settings: {
-					slidesToShow: 3,
-					slidesToScroll: 3,
-					infinite: true,
-					dots: true
-				  }
-				},
-				{
-				  breakpoint: 600,
-				  settings: "unslick"
-				}				
-			]
-		});
-	};
-	if($('.scroll').length) {
-		$(".scroll").mCustomScrollbar({
-			axis:"x",
-			theme:"dark-thin",
-			autoExpandScrollbar:true,
-			advanced:{autoExpandHorizontalScroll:true}
-		});
-	};
-	
-	*/
-	
-	/* components */
-	
-	
 
 });
 
@@ -108,7 +56,7 @@ var handler = function(){
 $(window).bind('load', handler);
 $(window).bind('resize', handler);
 
-$(document).ready(function(){
+$(window).load(function(){
 	var windWidth = $(window).width();
 
 	if(windWidth < 768){
@@ -322,7 +270,7 @@ $(document).ready(function(){
 	});
 
 	//Mobile menu
-	$(".header__toogle").click(function(){
+	$(".header__toogle").on('click',function(){
 		$(".mobside").slideToggle();
 	});
 	$(".mobside_nav_toggle").click(function(){
