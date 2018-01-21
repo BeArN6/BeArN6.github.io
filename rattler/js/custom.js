4f4ecc6cf83f768d0bacb102e240563192f34fd8
BeArN6 <oleh.savitskiy@gmail.com>
2018-01-21 08:39:23 +0200

    bgs

--- rattler/js/custom.js
+++ rattler/js/custom.js
@@ -150,7 +150,7 @@ $(window).load(function(){
 	}
 
 	$(".hamb").click(function(){
-		$("body, html").toggleClass("fixed");
+		$("body").toggleClass("fixed");
 		$(".sidebar").toggleClass("sidebar__toggle");
 		$(".banner").css({
 			"z-index":"3"
