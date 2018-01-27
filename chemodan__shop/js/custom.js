460e047457f42e876aa4b9ebd7fdb610b9969334
BeArN6 <oleh.savitskiy@gmail.com>
2018-01-27 13:13:26 +0200

    bgs

--- chemodan__shop/js/custom.js
+++ chemodan__shop/js/custom.js
@@ -859,6 +859,8 @@ $(window).load(function (){
 	$(".sp-thumbs").slick({
 		infinite: true,
   		slidesToShow: 4,
-  		slidesToScroll: 1
+  		slidesToScroll: 1,
+  		prevArrow:"<button type='button' class='slick-prev pull-left'><i class='fa fa-angle-left' aria-hidden='true'></i></button>",
+        nextArrow:"<button type='button' class='slick-next pull-right'><i class='fa fa-angle-right' aria-hidden='true'></button>"
 	});
 });
