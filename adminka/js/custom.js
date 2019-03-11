d4b2fdfd72b7e6b57e6a26877efb2ff05201bd6c
BeArN <oleh123091973@gmail.com>
2019-03-11 12:04:29 +0200

    adminka

--- adminka/js/custom.js
+++ adminka/js/custom.js
@@ -9,7 +9,9 @@ $(window).load(function(){
 	$('body').removeClass('loaded');
 
 	$('[data-toggle="datepicker"]').datepicker({
-		format: 'dd.mm.yyyy'
+		format: 'dd.mm.yyyy',
+		autoPick: true,
+		autoHide: true,
 	});
 
 	$(".header__toggle").click(function(e){
@@ -26,6 +28,9 @@ $(window).load(function(){
 	});
 
     $(".sidebar").mCustomScrollbar();
+    $(".withdropdown").click(function(){
+    	$(this).toggleClass("active");
+    });
 
 	/*Rightbar*/
 	$(".rightbar__tab").click(function(){
@@ -45,16 +50,24 @@ $(window).load(function(){
 	    var tH = (window.orientation === 0 ? screen.height : screen.width) -  getIOSWindowHeight();
 	    return tH > 1 ? tH : 0;
 	};
-	if(windWidth < 768){
-		$(".rightbar").height(getIOSWindowHeight() - $(".header").outerHeight());
-		$(".sidebar").height(getIOSWindowHeight());
 
+	if(windWidth < 1201){
 		$(document).mouseup(function (e) {
 		    var container = $(".sidebar");
 		    if (container.has(e.target).length === 0){
 		        container.removeClass("active");
+		        $(".profile").removeClass("active");
 		    }
 		});
+	}
+
+	if(windWidth < 768){
+		$(".sidebar__close").click(function(){
+			$(".sidebar, .profile").removeClass("active");
+		});
+		
+		$(".rightbar").height(getIOSWindowHeight() - $(".header").outerHeight());
+		$(".sidebar").height(getIOSWindowHeight());
 
 		$(window).scroll(function() {
     		let headerHeight = $(".header").outerHeight();
@@ -93,4 +106,50 @@ $(window).load(function(){
 		);
 	});
 	/*Profile end*/
+
+	/*Graph*/
+	var chart = new CanvasJS.Chart("chartContainer", {
+		animationEnabled: true,
+		axisX: {
+			valueFormatString: "DD MMM",
+			labelFontFamily: "Conv_Poppins",
+			labelFontColor: "#666666",
+			labelFontSize: 14
+		},
+		axisY: {
+			labelFontFamily: "Conv_Poppins",
+			labelFontColor: "#666666",
+			labelFontSize: 14
+		},
+		toolTip: {
+			fontFamily: "Conv_Poppins",
+			fontSize: 12,
+			fontColor: "#333333",
+			borderColor: "transparent",
+			cornerRadius: 5
+		},
+		data: [{
+			type: "splineArea",
+			color: "rgba(63,213,194,1)",
+			markerSize: 12,
+			xValueFormatString: "DD MMM",
+			markerBorderColor: "#ffffff",
+			markerBorderThickness: 2,
+			dataPoints: [
+				{ x: new Date(2019, 3, 7), y: 328 },
+				{ x: new Date(2019, 3, 8), y: 328 },
+				{ x: new Date(2019, 3, 9), y: 383 },
+				{ x: new Date(2019, 3, 10), y: 200 },
+				{ x: new Date(2019, 3, 11), y: 284 },
+				{ x: new Date(2019, 3, 12), y: 239 },
+				{ x: new Date(2019, 3, 13), y: 161 },
+				{ x: new Date(2019, 3, 14), y: 282 },
+				{ x: new Date(2019, 3, 15), y: 200 },
+				{ x: new Date(2019, 3, 16), y: 140 },
+				{ x: new Date(2019, 3, 17), y: 231 },
+				{ x: new Date(2019, 3, 18), y: 282 },
+			]
+		}]
+	});
+	chart.render();
 });
